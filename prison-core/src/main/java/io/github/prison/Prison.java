e6b20dafd47643ec39c871e924c91aba14af74c5
SirFaizdat <fdatoo7@gmail.com>
2016-06-20 03:21:08 -0400

    The Prison singleton

---
+++ prison-core/src/main/java/io/github/prison/Prison.java
@@ -0,0 +1,55 @@
+/*
+ *  Prison is a Minecraft plugin for the prison game mode.
+ *  Copyright (C) 2016 The Prison Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.prison;
+
+/**
+ * Entry point for implementations.
+ *
+ * @author SirFaizdat
+ * @since 3.0
+ */
+public class Prison {
+
+    // Singleton
+
+    private static Prison instance = null;
+
+    public static Prison getInstance() {
+        if (instance == null) instance = new Prison();
+        return instance;
+    }
+
+    // Public methods
+
+    /**
+     * Initializes prison-core.
+     * In the implementations, this should be called when the plugin is enabled.
+     */
+    public void init() {
+        // TODO This will eventually take in the Platform instance and store it
+    }
+
+    /**
+     * Finalizes and unregisters instances in prison-core.
+     * In the implementations, this should be called when the plugin is disabled.
+     */
+    public void deinit() {
+    }
+
+}
---
+++ prison-spigot/src/main/java/io/github/prison/spigot/SpigotPrison.java
@@ -0,0 +1,41 @@
+/*
+ *  Prison is a Minecraft plugin for the prison game mode.
+ *  Copyright (C) 2016 The Prison Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.prison.spigot;
+
+import io.github.prison.Prison;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The plugin class for the Spigot implementation.
+ *
+ * @author SirFaizdat
+ */
+public class SpigotPrison extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Prison.getInstance().init();
+    }
+
+    @Override
+    public void onDisable() {
+        Prison.getInstance().deinit();
+    }
+
+}
