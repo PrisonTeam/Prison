15774e53fb46736793ab9a68d168945e62c2552b
DMP9Labs <dylanperks23@gmail.com>
2017-01-03 18:43:09 +0000

    Remove PrisonCells (Module split)

--- prison-cells/src/main/java/tech/mcprison/prison/cells/Cell.java
+++
@@ -1,7 +0,0 @@
-package tech.mcprison.prison.cells;
-
-/**
- * Created by DMP9 on 01/01/2017.
- */
-public class Cell {
-}
--- prison-cells/src/main/java/tech/mcprison/prison/cells/CellsFilter.java
+++
@@ -1,8 +0,0 @@
-package tech.mcprison.prison.cells;
-
-/**
- * Created by DMP9 on 01/01/2017.
- */
-public interface CellsFilter {
-    boolean accept(Cell c);
-}
