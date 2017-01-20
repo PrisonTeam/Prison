f332e3750b86cfbc67c8e3dc2071b64205b99ab3
DMP9Labs <dylanperks23@gmail.com>
2017-01-20 21:31:22 +0000

    Fix exception

--- prison-core/src/main/java/tech/mcprison/prison/util/ItemManager.java
+++ prison-core/src/main/java/tech/mcprison/prison/util/ItemManager.java
@@ -16,30 +16,37 @@ import java.util.HashMap;
  */
 public class ItemManager {
     private HashMap<BlockType,String> items;
-    public ItemManager() throws IOException {
-        File file = new File(Prison.get().getDataFolder(),"/items.csv");
-        items = new HashMap<>();
-        InputStream is;
-        if (!file.exists()){
-            InputStream inputStream = getClass().getResourceAsStream("/items.csv");
-            Files.copy(inputStream,Paths.get(file.getPath()));
-        }
-        BufferedReader in = new BufferedReader(new FileReader(file));
-        String inputLine;
-
-        while ((inputLine = in.readLine()) != null) {
-            try {
-                if (!inputLine.startsWith("#")) {
-                    String[] array = inputLine.split(",");
-                    String itemName = array[0];
-                    int id = Integer.parseInt(array[1]);
-                    short data = Short.parseShort(array[2]);
-                    items.put(BlockType.getBlockWithData(id, data), itemName);
-                }
-            }catch(Exception e){
-                Output.get().logError("Error while reading items.csv -- it's probably invalid",e);
-                continue;
+    public ItemManager() {
+        try {
+            File file = new File(Prison.get().getDataFolder(), "/items.csv");
+            items = new HashMap<>();
+            InputStream is;
+            if (!file.exists()) {
+                InputStream inputStream = getClass().getResourceAsStream("/items.csv");
+                Files.copy(inputStream, Paths.get(file.getPath()));
             }
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                try {
+                    if (!inputLine.startsWith("#")) {
+                        String[] array = inputLine.split(",");
+                        String itemName = array[0];
+                        int id = Integer.parseInt(array[1]);
+                        short data = Short.parseShort(array[2]);
+                        items.put(BlockType.getBlockWithData(id, data), itemName);
+                    }
+                } catch (Exception e) {
+                    Output.get()
+                        .logError("Error while reading items.csv -- it's probably invalid", e);
+                    continue;
+                }
+            }
+        }
+        catch (Exception e){
+            Output.get()
+                .logError("Error while reading items.csv", e);
         }
     }
     public HashMap<BlockType,String> getItems(){
