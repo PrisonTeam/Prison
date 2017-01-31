270b3c7daa25543730677f3a01fbec2df798bcd2
DMP9 <dylanperks23@gmail.com>
2017-01-31 21:19:33 +0000

    Gamemode system (#18)
    * Gamemode
    
    * Documentation for Gamemode

---
+++ prison-core/src/main/java/tech/mcprison/prison/util/Gamemode.java
@@ -0,0 +1,23 @@
+package tech.mcprison.prison.util;
+
+/**
+ * Represents the minecraft in-game gamemodes
+ */
+public enum Gamemode {
+    /**
+     * Represents the survival gamemode (gamemode 0)
+     */
+    SURVIVAL,
+    /**
+     * Represents the creative gamemode (gamemode 1)
+     */
+    CREATIVE,
+    /**
+     * Represents the adventure gamemode (gamemode 2)
+     */
+    ADVENTURE,
+    /**
+     * Represents the spectator gamemode (gamemode 3)
+     */
+    SPECTATOR,
+}
