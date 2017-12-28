c22d3970e4c2b1124e7acd2f04859fb44b01f1ef
Faizaan Datoo <fdatoo7@gmail.com>
2017-12-28 08:01:48 -0500

    Update dependencies

--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/permissions/LuckPermissions.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/permissions/LuckPermissions.java
@@ -1,15 +1,13 @@
 package tech.mcprison.prison.spigot.permissions;
 
+import java.util.UUID;
 import me.lucko.luckperms.LuckPerms;
+import me.lucko.luckperms.api.DataMutateResult;
 import me.lucko.luckperms.api.LuckPermsApi;
 import me.lucko.luckperms.api.Node;
 import me.lucko.luckperms.api.User;
-import me.lucko.luckperms.exceptions.ObjectAlreadyHasException;
 import tech.mcprison.prison.integration.PermissionIntegration;
 import tech.mcprison.prison.internal.Player;
-
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Faizaan A. Datoo
@@ -22,57 +20,61 @@ public class LuckPermissions implements PermissionIntegration {
         api = LuckPerms.getApi();
     }
 
-    @Override public void addPermission(Player holder, String permission) {
+    @Override
+    public void addPermission(Player holder, String permission) {
         editPermission(holder.getUUID(), permission, true);
     }
 
-    @Override public void removePermission(Player holder, String permission) {
+    @Override
+    public void removePermission(Player holder, String permission) {
         editPermission(holder.getUUID(), permission, false);
     }
 
-    private void editPermission(UUID uuid, String permission, boolean value) {
-        // load the user in from storage. we can specify "null" for their username,
-        // since it's unknown to us.
-        api.getStorage().loadUser(uuid, "null").thenComposeAsync(success -> {
-            // loading the user failed, return straight away
-            if (!success) {
-                return CompletableFuture.completedFuture(false);
-            }
+    private void editPermission(UUID uuid, String permission, boolean add) {
+        // get the user
+        User user = api.getUser(uuid);
+        if (user == null) {
+            return; // user not loaded
+        }
 
-            // get the user instance, they're now loaded in memory.
-            User user = api.getUser(uuid);
+        // build the permission node
+        Node node = api.getNodeFactory().newBuilder(permission).build();
 
-            // Build the permission node we want to set
-            Node node = api.getNodeFactory().newBuilder(permission).setValue(value).build();
+        // set the permission
+        DataMutateResult result;
+        if(add) {
+            result = user.setPermission(node);
+        } else {
+            result = user.unsetPermission(node);
+        }
 
-            // Set the permission, and return true if the user didn't already have it set.
-            try {
-                user.setPermission(node);
+        // wasn't successful.
+        // they most likely already have (or didn't have if add = false) the permission
+        if (result != DataMutateResult.SUCCESS) {
+            return;
+        }
 
-                // now we've set the permission, but still need to save the user data
-                // back to the storage.
+        // now, before we return, we need to have the user to storage.
+        // this method will save the user, then run the callback once complete.
+        api.getStorage().saveUser(user)
+            .thenAcceptAsync(wasSuccessful -> {
+                if (!wasSuccessful) {
+                    return;
+                }
 
-                // first save the user
-                return api.getStorage().saveUser(user)
-                    .thenCompose(b -> {
-                        // then cleanup their user instance so we don't create
-                        // a memory leak.
-                        api.cleanupUser(user);
-                        return CompletableFuture.completedFuture(b);
-                    });
+                // refresh the user's permissions, so the change is "live"
+                user.refreshCachedData();
 
-            } catch (ObjectAlreadyHasException e) {
-                return CompletableFuture.completedFuture(false);
-            }
-
-        }, api.getStorage().getAsyncExecutor());
+            }, api.getStorage().getAsyncExecutor());
     }
 
-    @Override public String getProviderName() {
+    @Override
+    public String getProviderName() {
         return "LuckPerms";
     }
 
-    @Override public boolean hasIntegrated() {
+    @Override
+    public boolean hasIntegrated() {
         return api != null;
     }
 
