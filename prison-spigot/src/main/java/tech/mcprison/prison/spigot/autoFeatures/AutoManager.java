e23a7427099e38d3c9e0150ccece544b40e10519
GABRYCA <gabrycaretti@gmail.com>
2020-04-09 13:34:53 +0200

    Changes to the AutoManager, "Disabled by default inside the code by areEnaledFeatures boolean"
    TO-DO:
    + Prison Tasks, delayed (will be for another beta)
    
    Changelogs:
    + Added many new booleans, in the future they'll be into configs
    + Fixed an issue when using AutoPickup with fortune on ores
    + Code changes
    
    P.S. Mine Block GUI have issues... don't work

--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/autoFeatures/AutoManager.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/autoFeatures/AutoManager.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import tech.mcprison.prison.spigot.SpigotPrison;
 
 /**
  * @author GABRYCA
@@ -18,11 +19,35 @@ public class AutoManager implements Listener {
         // Change this to true to enable these features
         // For now they aren't tested and will be disabled by default
         // Maybe we'll add them in a config
-        boolean areEnabledFeatures = false;
+        boolean AreEnabledFeatures = true;
+        boolean DropItemsIfInventoryIsFull = false;
 
-        if (areEnabledFeatures) {
+        if (AreEnabledFeatures) {
+
             // Get the player
             Player p = e.getPlayer();
+
+            // AutoPickup booleans
+            boolean AutoPickupEnabled = true;
+            boolean AutoPickupAllBlocks = true;
+            boolean AutoPickupGoldOre = true;
+            boolean AutoPickupIronOre = true;
+            boolean AutoPickupCoalOre = true;
+
+            // AutoSmelt booleans
+            boolean AutoSmeltEnabled = true;
+            boolean AutoSmeltGoldOre = true;
+            boolean AutoSmeltIronOre = true;
+
+            // AutoBlock booleans
+            boolean AutoBlockEnabled = true;
+            boolean AutoBlockGoldBlock = true;
+            boolean AutoBlockIronBlock = true;
+            boolean AutoBlockCoalBlock = true;
+            boolean AutoBlockDiamondBlock = true;
+            boolean AutoBlockRedstoneBlock = true;
+            boolean AutoBlockEmeraldBlock = true;
+            boolean AutoBlockQuartzBlock = true;
 
             // If the block is null somehow, return
             if (e.getBlock().getType() == null) {
@@ -33,83 +58,151 @@ public class AutoManager implements Listener {
             Material brokenBlock = e.getBlock().getType();
             String blockName = brokenBlock.toString().toLowerCase();
 
-            // AutoSmelt and autoPickup
-            switch (blockName) {
-                case "gold_ore":
-                    // Add the item to the inventory
-                    p.getInventory().addItem(new ItemStack(Material.GOLD_INGOT));
+            // Check if the inventory's full
+            if (p.getInventory().firstEmpty() == -1){
+
+                // Drop items when full
+                if (DropItemsIfInventoryIsFull) {
+
+                    p.sendMessage(SpigotPrison.format("&cWARNING! Your inventory's full and you're dropping items!"));
+
+                } else if (!(DropItemsIfInventoryIsFull)){ // Lose items when full
+
+                    p.sendMessage(SpigotPrison.format("&CWARNING! Your inventory's full and you're losing items!"));
 
                     // Set the broken block to AIR and cancel the event
                     e.setCancelled(true);
                     e.getBlock().setType(Material.AIR);
-                    break;
-                case "iron_ore":
-                    // Add the item to the inventory
-                    p.getInventory().addItem(new ItemStack(Material.IRON_INGOT));
 
-                    // Set the broken block to AIR and cancel the event
-                    e.setCancelled(true);
-                    e.getBlock().setType(Material.AIR);
-                    break;
-                case "coal_ore":
-                    // Add the item to the inventory
-                    p.getInventory().addItem(new ItemStack(Material.COAL));
+                }
+                return;
+            }
 
-                    // Set the broken block to AIR and cancel the event
-                    e.setCancelled(true);
-                    e.getBlock().setType(Material.AIR);
-                    break;
-                default:
+            // AutoPickup
+            if (AutoPickupEnabled) {
+                switch (blockName) {
+                    case "gold_ore":
 
-                    // Check if drops aren't null
-                    if (!(e.getBlock().getDrops() == null)) {
+                        if (AutoPickupGoldOre) {
 
-                        // Add the dropped items in a collection
-                        ItemStack[] drops = e.getBlock().getDrops().toArray(new ItemStack[0]);
+                            // Add the item to the inventory
+                            p.getInventory().addItem(e.getBlock().getDrops().toArray(new ItemStack[0]));
 
-                        // Add the items to the inventory
-                        p.getInventory().addItem(drops);
+                            // Set the broken block to AIR and cancel the event
+                            e.setCancelled(true);
+                            e.getBlock().setType(Material.AIR);
+                        }
+
+                        break;
+                    case "iron_ore":
+
+                        if (AutoPickupIronOre) {
+
+                            // Add the item to the inventory
+                            p.getInventory().addItem(e.getBlock().getDrops().toArray(new ItemStack[0]));
+
+                            // Set the broken block to AIR and cancel the event
+                            e.setCancelled(true);
+                            e.getBlock().setType(Material.AIR);
+                        }
+
+                        break;
+                    case "coal_ore":
+
+                        if (AutoPickupCoalOre) {
+
+                            // Add the item to the inventory
+                            p.getInventory().addItem(e.getBlock().getDrops().toArray(new ItemStack[0]));
+
+                            // Set the broken block to AIR and cancel the event
+                            e.setCancelled(true);
+                            e.getBlock().setType(Material.AIR);
+                        }
+
+                        break;
+                    default:
+
+                        if (AutoPickupAllBlocks) {
+                            // Check if drops aren't null
+                            if (!(e.getBlock().getDrops() == null)) {
+
+                                // Add the items to the inventory
+                                p.getInventory().addItem(e.getBlock().getDrops().toArray(new ItemStack[0]));
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            // AutoSmelt
+            if (AutoSmeltEnabled){
+                if (AutoSmeltGoldOre){
+                    while (p.getInventory().contains(Material.GOLD_ORE)){
+                        p.getInventory().removeItem(new ItemStack(Material.GOLD_ORE, 1));
+                        p.getInventory().addItem(new ItemStack(Material.GOLD_INGOT,1));
                     }
-
-                    break;
+                } else if (AutoSmeltIronOre){
+                    while (p.getInventory().contains(Material.IRON_ORE)){
+                        p.getInventory().removeItem(new ItemStack(Material.IRON_ORE, 1));
+                        p.getInventory().addItem(new ItemStack(Material.IRON_INGOT,1));
+                    }
+                }
             }
 
             // AutoBlock
-            if (p.getInventory().contains(Material.GOLD_INGOT, 9)) {
+            if (AutoBlockEnabled) {
+                if (p.getInventory().contains(Material.GOLD_INGOT, 9)) {
 
-                p.getInventory().removeItem(new ItemStack(Material.GOLD_INGOT, 9));
-                p.getInventory().addItem(new ItemStack(Material.GOLD_BLOCK));
+                    if (AutoBlockGoldBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.GOLD_INGOT, 9));
+                        p.getInventory().addItem(new ItemStack(Material.GOLD_BLOCK));
+                    }
 
-            } else if (p.getInventory().contains(Material.IRON_INGOT, 9)) {
+                } else if (p.getInventory().contains(Material.IRON_INGOT, 9)) {
 
-                p.getInventory().removeItem(new ItemStack(Material.IRON_INGOT, 9));
-                p.getInventory().addItem(new ItemStack(Material.IRON_BLOCK));
 
-            } else if (p.getInventory().contains(Material.COAL, 9)) {
+                    if (AutoBlockIronBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.IRON_INGOT, 9));
+                        p.getInventory().addItem(new ItemStack(Material.IRON_BLOCK));
+                    }
 
-                p.getInventory().removeItem(new ItemStack(Material.COAL, 9));
-                p.getInventory().addItem(new ItemStack(Material.COAL_BLOCK));
+                } else if (p.getInventory().contains(Material.COAL, 9)) {
 
-            } else if (p.getInventory().contains(Material.DIAMOND, 9)) {
+                    if (AutoBlockCoalBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.COAL, 9));
+                        p.getInventory().addItem(new ItemStack(Material.COAL_BLOCK));
+                    }
 
-                p.getInventory().removeItem(new ItemStack(Material.DIAMOND, 9));
-                p.getInventory().addItem(new ItemStack(Material.DIAMOND_BLOCK));
+                } else if (p.getInventory().contains(Material.DIAMOND, 9)) {
 
-            } else if (p.getInventory().contains(Material.REDSTONE, 9)) {
+                    if (AutoBlockDiamondBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.DIAMOND, 9));
+                        p.getInventory().addItem(new ItemStack(Material.DIAMOND_BLOCK));
+                    }
 
-                p.getInventory().removeItem(new ItemStack(Material.REDSTONE, 9));
-                p.getInventory().addItem(new ItemStack(Material.REDSTONE_BLOCK));
+                } else if (p.getInventory().contains(Material.REDSTONE, 9)) {
 
-            } else if (p.getInventory().contains(Material.EMERALD, 9)) {
+                    if (AutoBlockRedstoneBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.REDSTONE, 9));
+                        p.getInventory().addItem(new ItemStack(Material.REDSTONE_BLOCK));
+                    }
 
-                p.getInventory().removeItem(new ItemStack(Material.EMERALD, 9));
-                p.getInventory().addItem(new ItemStack(Material.EMERALD_BLOCK));
+                } else if (p.getInventory().contains(Material.EMERALD, 9)) {
 
-            } else if (p.getInventory().contains(Material.QUARTZ, 9)) {
+                    if (AutoBlockEmeraldBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.EMERALD, 9));
+                        p.getInventory().addItem(new ItemStack(Material.EMERALD_BLOCK));
+                    }
 
-                p.getInventory().removeItem(new ItemStack(Material.QUARTZ, 9));
-                p.getInventory().addItem(new ItemStack(Material.QUARTZ_BLOCK));
+                } else if (p.getInventory().contains(Material.QUARTZ, 9)) {
 
+                    if (AutoBlockQuartzBlock) {
+                        p.getInventory().removeItem(new ItemStack(Material.QUARTZ, 9));
+                        p.getInventory().addItem(new ItemStack(Material.QUARTZ_BLOCK));
+                    }
+
+                }
             }
 
         }
