5b22441b328fd3835c9e7df29eecb1a9c571ba73
gabrielecaretti <gabrycaretti@gmail.com>
2020-06-02 16:43:00 +0200

    Minor changes to prison gui
    Changelogs:
    - Edited the rankprice blocks number because they make confusion

--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/gui/rank/SpigotRankPriceGUI.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/gui/rank/SpigotRankPriceGUI.java
@@ -63,15 +63,15 @@ public class SpigotRankPriceGUI extends SpigotGUIComponents {
         inv.setItem(1, decreaseOf1);
 
         // Decrease button
-        ItemStack decreaseOf5 = createButton(Material.REDSTONE_BLOCK, 5, changeDecreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " - 10"));
+        ItemStack decreaseOf5 = createButton(Material.REDSTONE_BLOCK, 10, changeDecreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " - 10"));
         inv.setItem(10, decreaseOf5);
 
         // Decrease button
-        ItemStack decreaseOf10 = createButton(Material.REDSTONE_BLOCK, 10, changeDecreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " - 100"));
+        ItemStack decreaseOf10 = createButton(Material.REDSTONE_BLOCK, 1, changeDecreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " - 100"));
         inv.setItem(19, decreaseOf10);
 
         // Decrease button
-        ItemStack decreaseOf50 = createButton(Material.REDSTONE_BLOCK, 50, changeDecreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " - 1000"));
+        ItemStack decreaseOf50 = createButton(Material.REDSTONE_BLOCK, 1, changeDecreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " - 1000"));
         inv.setItem(28, decreaseOf50);
 
         // Decrease button
@@ -91,15 +91,15 @@ public class SpigotRankPriceGUI extends SpigotGUIComponents {
         inv.setItem(7, increseOf1);
 
         // Increase button
-        ItemStack increaseOf5 = createButton(Material.EMERALD_BLOCK, 5, changeIncreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " + 10"));
+        ItemStack increaseOf5 = createButton(Material.EMERALD_BLOCK, 10, changeIncreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " + 10"));
         inv.setItem(16, increaseOf5);
 
         // Increase button
-        ItemStack increaseOf10 = createButton(Material.EMERALD_BLOCK, 10, changeIncreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " + 100"));
+        ItemStack increaseOf10 = createButton(Material.EMERALD_BLOCK, 1, changeIncreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " + 100"));
         inv.setItem(25, increaseOf10);
 
         // Increase button
-        ItemStack increaseOf50 = createButton(Material.EMERALD_BLOCK, 50, changeIncreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " + 1000"));
+        ItemStack increaseOf50 = createButton(Material.EMERALD_BLOCK, 1, changeIncreaseValueLore, SpigotPrison.format("&3" + rankName + " " + val + " + 1000"));
         inv.setItem(34, increaseOf50);
 
         // Increase button
