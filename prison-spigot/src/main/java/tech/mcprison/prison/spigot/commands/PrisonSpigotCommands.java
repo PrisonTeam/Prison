e48789a99d413271c41e945b257b4f6389d1df2d
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2020-06-21 13:48:47 -0400

    Clean up dead imports to eliminate complie warnings.

--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/PrisonSpigotCommands.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/PrisonSpigotCommands.java
@@ -1,19 +1,15 @@
 package tech.mcprison.prison.spigot.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import tech.mcprison.prison.spigot.SpigotPrison;
 import tech.mcprison.prison.spigot.gui.SpigotPrisonGUI;
-import tech.mcprison.prison.spigot.gui.rank.SpigotPlayerPrestigesGUI;
 import tech.mcprison.prison.spigot.gui.mine.SpigotPlayerMinesGUI;
+import tech.mcprison.prison.spigot.gui.rank.SpigotPlayerPrestigesGUI;
 import tech.mcprison.prison.spigot.gui.rank.SpigotPlayerRanksGUI;
 import tech.mcprison.prison.spigot.spiget.BluesSpigetSemVerComparator;
 
