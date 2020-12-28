28939c02a5e28d332807465ac0242ddcf1d4d1e8
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2020-12-27 19:57:56 -0500

    Oof... forgot to commit the PlaceholderAttributeBar class!  This will be needed for the complier... lol

---
+++ prison-core/src/main/java/tech/mcprison/prison/placeholders/PlaceholderAttributeBar.java
@@ -0,0 +1,97 @@
+package tech.mcprison.prison.placeholders;
+
+import tech.mcprison.prison.output.Output;
+
+public class PlaceholderAttributeBar
+		implements PlaceholderAttribute {
+	
+	private String[] parts;
+	
+	private PlaceholderProgressBarConfig barConfig;
+	
+	private boolean debug = false;
+	
+	public PlaceholderAttributeBar( String[] parts, PlaceholderProgressBarConfig defaultBarConfig ) {
+		super();
+
+		this.parts = parts;
+
+		// ::bar:size:posColor:posSeg:negColor:negSeg:debug
+		
+		int len = 1;
+		
+		// format:
+		String segStr = parts.length > len ? parts[len++] : null;
+		int seg = defaultBarConfig.getSegments();
+		if ( segStr != null && !segStr.trim().isEmpty() ) {
+			
+			try {
+				seg = Integer.parseInt( segStr );
+			}
+			catch (NumberFormatException e ) {
+				// invalid supplied segments. 
+				// Ignore unless in debug mode:
+				if ( isDebug() ) {
+					Output.get().logError( 
+							String.format( "Error parsing segments to an Integer. String value= [%s] " +
+									"ERRROR: %s", 
+									
+									segStr, e.getMessage()
+									));
+				}
+
+			}
+		}
+		
+
+		String pCol = parts.length > len ? parts[len++] : defaultBarConfig.getPositiveColor();
+		
+		String pSeg = parts.length > len ? parts[len++] : defaultBarConfig.getPositiveSegment();
+		
+		String nCol = parts.length > len ? parts[len++] : defaultBarConfig.getNegativeColor();
+
+		String nSeg = parts.length > len ? parts[len++] : defaultBarConfig.getNegativeSegment();
+
+		this.barConfig = new PlaceholderProgressBarConfig( seg, pCol, pSeg, nCol, nSeg );
+		
+		// Debug:
+		String debugStr = parts.length > len ? parts[len++] : null;
+		boolean debug = debugStr != null && "debug".equalsIgnoreCase( debugStr );
+		
+		
+		this.debug = debug;
+
+	}
+
+
+	public String[] getParts() {
+		return parts;
+	}
+	public void setParts( String[] parts ) {
+		this.parts = parts;
+	}
+
+	public boolean isDebug() {
+		return debug;
+	}
+	public void setDebug( boolean debug ) {
+		this.debug = debug;
+	}
+
+	public PlaceholderProgressBarConfig getBarConfig() {
+		return barConfig;
+	}
+	public void setBarConfig( PlaceholderProgressBarConfig barConfig ) {
+		this.barConfig = barConfig;
+	}
+
+	@Override
+	public String format( String value )
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
