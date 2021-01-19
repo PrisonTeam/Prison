2bbedd6dbd2599ef3fc7019c311ff02ead5584e4
GABRYCA <gabrycaretti@gmail.com>
2021-01-19 20:06:07 +0100

    SellAll XMaterial better implementation.
    Changelogs:
    - Fixed SellAll (tested PaperSpigot 1.16.4 and it's working now).
    - Using XMaterial as a compare method (code insight), Converting ItemStacks to XMaterial, which's going to be the new PrisonBlock in the future maybe.

--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/sellall/SellAllPrisonCommands.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/sellall/SellAllPrisonCommands.java
@@ -77,6 +77,205 @@ public class SellAllPrisonCommands extends PrisonSpigotBaseCommands {
         activePlayerDelay.remove(p.getName());
     }
 
+    /**
+     * Get the money to give to the Player depending on the multiplier.
+     * */
+    private double getMoneyWithMultiplier(SpigotPlayer sPlayer) {
+
+        // Get the Items config section
+        Set<String> items = sellAllConfig.getConfigurationSection("Items").getKeys(false);
+        moneyToGive = 0;
+
+        // Get money to give
+        moneyToGive = getMoneyToGiveRemoveItems( sPlayer.getWrapper(), items, moneyToGive);
+
+        if (sellAllConfig.getString("Options.Multiplier_Enabled").equalsIgnoreCase("true")) {
+
+            moneyToGive = moneyToGive * getMultiplier(sPlayer);;
+        }
+
+        return moneyToGive;
+    }
+
+    /**
+     * Get the money to give to the Player depending on the multiplier.
+     * */
+    public double getMoneyWithMultiplier(Player player){
+
+        SpigotPlayer sPlayer = new SpigotPlayer(player);
+
+        // Get the Items config section
+        Set<String> items = sellAllConfig.getConfigurationSection("Items").getKeys(false);
+        moneyToGive = 0;
+
+        // Get money to give
+        moneyToGive = getMoneyToGive( sPlayer.getWrapper(), items, moneyToGive);
+
+        if (sellAllConfig.getString("Options.Multiplier_Enabled").equalsIgnoreCase("true")) {
+
+            moneyToGive = moneyToGive * getMultiplier(sPlayer);;
+        }
+
+        return moneyToGive;
+    }
+
+    /**
+     * Get player multiplier, this uses the normal Player and actually return a double.
+     * */
+    public double getMultiplier(Player player){
+
+        // Get Spigot Player
+        SpigotPlayer sPlayer = new SpigotPlayer(player);
+
+        return getMultiplier(sPlayer);
+    }
+
+    /**
+     * Get the player multiplier, requires SpigotPlayer.
+     * */
+    public double getMultiplier(SpigotPlayer sPlayer) {
+
+        // Get Ranks module.
+        ModuleManager modMan = Prison.get().getModuleManager();
+        Module module = modMan == null ? null : modMan.getModule( PrisonRanks.MODULE_NAME ).orElse( null );
+
+        // Get default multiplier
+        double multiplier = Double.parseDouble(sellAllConfig.getString("Options.Multiplier_Default"));
+
+        // Get multiplier depending on Player + Prestige. NOTE that prestige multiplier will replace
+        // the actual default multiplier.
+        if (module != null) {
+            PrisonRanks rankPlugin = (PrisonRanks) module;
+
+            if (rankPlugin.getPlayerManager().getPlayer(sPlayer.getUUID(), sPlayer.getName()).isPresent()) {
+                String playerRankName;
+                try {
+                    playerRankName = rankPlugin.getPlayerManager().getPlayer(sPlayer.getUUID(), sPlayer.getName()).get().getRank("prestiges").getName();
+                } catch (NullPointerException ex){
+                    playerRankName = null;
+                }
+                if ((playerRankName != null) && (sellAllConfig.getString("Multiplier." + playerRankName + ".MULTIPLIER") != null)) {
+                    multiplier = Double.parseDouble(sellAllConfig.getString("Multiplier." + playerRankName + ".MULTIPLIER"));
+                }
+            }
+        }
+        List<String> perms = sPlayer.getPermissions("prison.sellall.multiplier.");
+        double multiplierExtraByPerms = 0;
+        for (String multByPerm : perms){
+            double multByPermDouble = Double.parseDouble(multByPerm.substring(26));
+            if (!sellAllConfig.getString("Options.Multiplier_Permission_Only_Higher").equalsIgnoreCase("true")) {
+                multiplierExtraByPerms += multByPermDouble;
+            } else if (sellAllConfig.getString("Options.Multiplier_Permission_Only_Higher").equalsIgnoreCase("true") && multByPermDouble > multiplierExtraByPerms){
+                multiplierExtraByPerms = multByPermDouble;
+            }
+        }
+        multiplier += multiplierExtraByPerms;
+
+        return multiplier;
+    }
+
+    /**
+     * Get value of the items in Player's Inventory if SellAll's triggered now.
+     * */
+    public double getMoneyToGive(Player p, Set<String> items, double moneyToGive) {
+
+        // Get the player inventory
+        Inventory inv = p.getInventory();
+
+        // Get the items from the player inventory and for each of them check the conditions.
+        for (ItemStack itemStack : inv.getContents()){
+
+            if (itemStack != null) {
+                // Get the items strings from config and for each of them get the Material and value.
+                for (String key : items) {
+                    ItemStack itemMaterial = XMaterial.valueOf(sellAllConfig.getString("Items." + key + ".ITEM_ID")).parseItem();
+                    double value = Double.parseDouble(sellAllConfig.getString("Items." + key + ".ITEM_VALUE"));
+                    int amount = 0;
+
+                    // Check if the item from the player inventory's on the config of items sellable
+                    // So it gets the amount and then remove it from the inventory
+                    if (itemMaterial != null && itemMaterial == itemStack) {
+                        amount = itemStack.getAmount();
+                    }
+                    // Get the new amount of money to give
+                    if (amount != 0) {
+                        moneyToGive = moneyToGive + (value * amount);
+                    }
+                }
+            }
+        }
+        return moneyToGive;
+    }
+
+    private double getMoneyToGiveRemoveItems(Player p, Set<String> items, double moneyToGive){
+        // Get the player inventory
+        Inventory inv = p.getInventory();
+
+        // Get the items from the player inventory and for each of them check the conditions.
+        for (ItemStack itemStack : inv.getContents()){
+
+            if (itemStack != null) {
+                // Get the items strings from config and for each of them get the Material and value.
+                for (String key : items) {
+
+                    boolean hasError = false;
+
+                    XMaterial itemMaterial = null;
+                    XMaterial invMaterial = null;
+                    try {
+                        itemMaterial = XMaterial.valueOf(sellAllConfig.getString("Items." + key + ".ITEM_ID"));
+                        invMaterial = XMaterial.matchXMaterial(itemStack);
+                    } catch (IllegalArgumentException ex){
+                        hasError = true;
+                    }
+
+                    double value = 0;
+                    try {
+                        value = Double.parseDouble(sellAllConfig.getString("Items." + key + ".ITEM_VALUE"));
+                    } catch (NumberFormatException ex){
+                        if (!hasError) hasError = true;
+                    }
+
+                    int amount = 0;
+                    // Check if the item from the player inventory's on the config of items sellable
+                    // So it gets the amount and then remove it from the inventory
+                    if (!hasError && itemMaterial == invMaterial) {
+                        amount = itemStack.getAmount();
+                        p.getInventory().remove(itemStack);
+                    }
+                    // Get the new amount of money to give
+                    if (amount != 0) {
+                        moneyToGive = moneyToGive + (value * amount);
+                    }
+                }
+            }
+        }
+        return moneyToGive;
+    }
+
+    private boolean sellAllPlayerGUI(Player p) {
+        // Check if the Player GUI's enabled.
+        if (sellAllConfig.getString("Options.Player_GUI_Enabled").equalsIgnoreCase("true")){
+            // Check if a permission's required, if not it'll open directly the Player's GUI.
+            if (sellAllConfig.getString("Options.Player_GUI_Permission_Enabled").equalsIgnoreCase("true")){
+                // Check if the sender has the required permission.
+                if (p.hasPermission("Options.Player_GUI_Permission")){
+                    SellAllPlayerGUI gui = new SellAllPlayerGUI(p);
+                    gui.open();
+                    // If missing will send a missing permission error message.
+                } else {
+                    p.sendMessage(SpigotPrison.format(messages.getString("Message.SellAllMissingPermission") + sellAllConfig.getString("Options.Player_GUI_Permission") + "]"));
+                }
+                // Because a permission isn't required, it'll open directly the GUI.
+            } else {
+                SellAllPlayerGUI gui = new SellAllPlayerGUI(p);
+                gui.open();
+            }
+            return true;
+        }
+        return false;
+    }
+
     @Command(identifier = "sellall", description = "SellAll main command", onlyPlayers = false)
     private void sellAllCommands(CommandSender sender) {
 
@@ -324,168 +523,6 @@ public class SellAllPrisonCommands extends PrisonSpigotBaseCommands {
         }
     }
 
-    /**
-     * Get the money to give to the Player depending on the multiplier.
-     * */
-    private double getMoneyWithMultiplier(SpigotPlayer sPlayer) {
-
-        // Get the Items config section
-        Set<String> items = sellAllConfig.getConfigurationSection("Items").getKeys(false);
-        moneyToGive = 0;
-
-        // Get money to give
-        moneyToGive = getMoneyToGiveRemoveItems( sPlayer.getWrapper(), items, moneyToGive);
-
-        if (sellAllConfig.getString("Options.Multiplier_Enabled").equalsIgnoreCase("true")) {
-
-            moneyToGive = moneyToGive * getMultiplier(sPlayer);;
-        }
-
-        return moneyToGive;
-    }
-
-    /**
-     * Get the money to give to the Player depending on the multiplier.
-     * */
-    public double getMoneyWithMultiplier(Player player){
-
-        SpigotPlayer sPlayer = new SpigotPlayer(player);
-
-        // Get the Items config section
-        Set<String> items = sellAllConfig.getConfigurationSection("Items").getKeys(false);
-        moneyToGive = 0;
-
-        // Get money to give
-        moneyToGive = getMoneyToGive( sPlayer.getWrapper(), items, moneyToGive);
-
-        if (sellAllConfig.getString("Options.Multiplier_Enabled").equalsIgnoreCase("true")) {
-
-            moneyToGive = moneyToGive * getMultiplier(sPlayer);;
-        }
-
-        return moneyToGive;
-    }
-
-    /**
-     * Get player multiplier, this uses the normal Player and actually return a double.
-     * */
-    public double getMultiplier(Player player){
-
-        // Get Spigot Player
-        SpigotPlayer sPlayer = new SpigotPlayer(player);
-
-        return getMultiplier(sPlayer);
-    }
-
-    /**
-     * Get the player multiplier, requires SpigotPlayer.
-     * */
-    public double getMultiplier(SpigotPlayer sPlayer) {
-
-        // Get Ranks module.
-        ModuleManager modMan = Prison.get().getModuleManager();
-        Module module = modMan == null ? null : modMan.getModule( PrisonRanks.MODULE_NAME ).orElse( null );
-
-        // Get default multiplier
-        double multiplier = Double.parseDouble(sellAllConfig.getString("Options.Multiplier_Default"));
-
-        // Get multiplier depending on Player + Prestige. NOTE that prestige multiplier will replace
-        // the actual default multiplier.
-        if (module != null) {
-        	PrisonRanks rankPlugin = (PrisonRanks) module;
-        	
-            if (rankPlugin.getPlayerManager().getPlayer(sPlayer.getUUID(), sPlayer.getName()).isPresent()) {
-                String playerRankName;
-                try {
-                    playerRankName = rankPlugin.getPlayerManager().getPlayer(sPlayer.getUUID(), sPlayer.getName()).get().getRank("prestiges").getName();
-                } catch (NullPointerException ex){
-                    playerRankName = null;
-                }
-                if ((playerRankName != null) && (sellAllConfig.getString("Multiplier." + playerRankName + ".MULTIPLIER") != null)) {
-                    multiplier = Double.parseDouble(sellAllConfig.getString("Multiplier." + playerRankName + ".MULTIPLIER"));
-                }
-            }
-        }
-        List<String> perms = sPlayer.getPermissions("prison.sellall.multiplier.");
-        double multiplierExtraByPerms = 0;
-        for (String multByPerm : perms){
-            double multByPermDouble = Double.parseDouble(multByPerm.substring(26));
-            if (!sellAllConfig.getString("Options.Multiplier_Permission_Only_Higher").equalsIgnoreCase("true")) {
-                multiplierExtraByPerms += multByPermDouble;
-            } else if (sellAllConfig.getString("Options.Multiplier_Permission_Only_Higher").equalsIgnoreCase("true") && multByPermDouble > multiplierExtraByPerms){
-                multiplierExtraByPerms = multByPermDouble;
-            }
-        }
-        multiplier += multiplierExtraByPerms;
-        
-        return multiplier;
-    }
-
-    /**
-     * Get value of the items in Player's Inventory if SellAll's triggered now.
-     * */
-    public double getMoneyToGive(Player p, Set<String> items, double moneyToGive) {
-
-        // Get the player inventory
-        Inventory inv = p.getInventory();
-
-        // Get the items from the player inventory and for each of them check the conditions.
-        for (ItemStack itemStack : inv.getContents()){
-
-            if (itemStack != null) {
-                // Get the items strings from config and for each of them get the Material and value.
-                for (String key : items) {
-                    ItemStack itemMaterial = XMaterial.valueOf(sellAllConfig.getString("Items." + key + ".ITEM_ID")).parseItem();
-                    double value = Double.parseDouble(sellAllConfig.getString("Items." + key + ".ITEM_VALUE"));
-                    int amount = 0;
-
-                    // Check if the item from the player inventory's on the config of items sellable
-                    // So it gets the amount and then remove it from the inventory
-                    if (itemMaterial != null && itemMaterial == itemStack) {
-                        amount = itemStack.getAmount();
-                    }
-                    // Get the new amount of money to give
-                    if (amount != 0) {
-                        moneyToGive = moneyToGive + (value * amount);
-                    }
-                }
-            }
-        }
-        return moneyToGive;
-    }
-
-    public double getMoneyToGiveRemoveItems(Player p, Set<String> items, double moneyToGive){
-        // Get the player inventory
-        Inventory inv = p.getInventory();
-
-        // Get the items from the player inventory and for each of them check the conditions.
-        for (ItemStack itemStack : inv.getContents()){
-
-            if (itemStack != null) {
-                // Get the items strings from config and for each of them get the Material and value.
-                for (String key : items) {
-                    ItemStack itemMaterial = XMaterial.valueOf(sellAllConfig.getString("Items." + key + ".ITEM_ID")).parseItem();
-                    double value = Double.parseDouble(sellAllConfig.getString("Items." + key + ".ITEM_VALUE"));
-                    int amount = 0;
-
-                    // Check if the item from the player inventory's on the config of items sellable
-                    // So it gets the amount and then remove it from the inventory
-                    if (itemMaterial != null && itemMaterial == itemStack) {
-                        amount = itemStack.getAmount();
-                        p.getInventory().remove(itemStack);
-                    }
-                    // Get the new amount of money to give
-                    if (amount != 0) {
-                        moneyToGive = moneyToGive + (value * amount);
-                    }
-                }
-            }
-        }
-        return moneyToGive;
-    }
-
-
-
     @Command(identifier = "sellall auto toggle", description = "Let the user enable or disable sellall auto", onlyPlayers = true)
     private void sellAllAutoEnableUser(CommandSender sender){
 
@@ -594,29 +631,6 @@ public class SellAllPrisonCommands extends PrisonSpigotBaseCommands {
         if (p.hasPermission("prison.admin")) {
             sender.sendMessage(SpigotPrison.format(messages.getString("Message.SellAllGUIDisabled")));
         }
-    }
-
-    private boolean sellAllPlayerGUI(Player p) {
-        // Check if the Player GUI's enabled.
-        if (sellAllConfig.getString("Options.Player_GUI_Enabled").equalsIgnoreCase("true")){
-            // Check if a permission's required, if not it'll open directly the Player's GUI.
-            if (sellAllConfig.getString("Options.Player_GUI_Permission_Enabled").equalsIgnoreCase("true")){
-                // Check if the sender has the required permission.
-                if (p.hasPermission("Options.Player_GUI_Permission")){
-                    SellAllPlayerGUI gui = new SellAllPlayerGUI(p);
-                    gui.open();
-                // If missing will send a missing permission error message.
-                } else {
-                    p.sendMessage(SpigotPrison.format(messages.getString("Message.SellAllMissingPermission") + sellAllConfig.getString("Options.Player_GUI_Permission") + "]"));
-                }
-            // Because a permission isn't required, it'll open directly the GUI.
-            } else {
-                SellAllPlayerGUI gui = new SellAllPlayerGUI(p);
-                gui.open();
-            }
-            return true;
-        }
-        return false;
     }
 
     @Command(identifier = "sellall add", description = "SellAll add an item to the sellAll shop.", onlyPlayers = false)
