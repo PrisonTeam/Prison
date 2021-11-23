df380b2b094b6a0f22abc5648aba8cf6d4f56ade
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2021-11-23 06:17:33 -0500

    Setup a mutex for locking the mine resets, which prevents active block break attempts, including explosions, from trying to break blocks while a mine is actively being reset.

---
+++ prison-mines/src/main/java/tech/mcprison/prison/mines/data/MineStateMutex.java
@@ -0,0 +1,99 @@
+package tech.mcprison.prison.mines.data;
+
+public class MineStateMutex
+{
+	private MineState mineState;
+	private int mineStateSn = 0;
+	
+	private boolean cloned = false;
+
+	public enum MineState {
+		LOCKED,
+		UNLOCKED;
+	}
+	
+	public MineStateMutex() {
+		
+		this.mineState = MineState.UNLOCKED;
+		
+		this.mineStateSn = 0;
+		
+		this.cloned = false;
+	}
+	
+	private MineStateMutex( MineStateMutex cloneSource ) {
+		
+		this.mineState = cloneSource.getMineState();
+		
+		this.mineStateSn = cloneSource.getMineStateSn();
+		
+		this.cloned = true;
+	}
+	
+	public void setMineStateResetStart() {
+		
+		synchronized ( this ) {
+			
+			this.mineState = MineState.LOCKED;
+			this.mineStateSn++;
+		}
+		
+	}
+	
+	public void setMineStateResetFinished() {
+		
+		synchronized ( this ) {
+			
+			this.mineState = MineState.UNLOCKED;
+		}
+		
+	}
+	
+	public boolean isMinable() {
+		boolean results = false;
+		
+		synchronized ( this ) {
+			
+			if ( getMineState() == MineState.UNLOCKED ) {
+				
+				results = true;
+			}
+		}
+		
+		return results;
+	}
+	
+	public boolean isValidState( MineStateMutex mutex ) {
+		boolean results = false;
+		
+		synchronized ( this ) {
+			
+			if ( getMineState() == MineState.UNLOCKED && 
+					getMineStateSn() == mutex.getMineStateSn() ) {
+				
+				results = true;
+			}
+		}
+		
+		return results;
+	}
+
+	public MineStateMutex clone() {
+		MineStateMutex results = new MineStateMutex( this );
+		
+		return results;
+	}
+	
+	public MineState getMineState() {
+		return mineState;
+	}
+
+	public int getMineStateSn() {
+		return mineStateSn;
+	}
+
+	public boolean isCloned() {
+		return cloned;
+	}
+	
+}
