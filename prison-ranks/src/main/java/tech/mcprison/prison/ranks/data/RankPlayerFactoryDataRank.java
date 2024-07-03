029a184400b24b02048d0cf58df9b8f5701134ef
rbluer <665978+rbluer@users.noreply.github.com>
2024-07-03 02:26:04 -0400

    RankPlayer: Added new fields to expand what is being stored in the RankPlayer object to store data that is not available when the player is offline. This can provide additional support for players when they are offline.

---
+++ prison-ranks/src/main/java/tech/mcprison/prison/ranks/data/RankPlayerFactoryDataRank.java
@@ -0,0 +1,54 @@
+package tech.mcprison.prison.ranks.data;
+
+import java.util.TreeMap;
+
+public class RankPlayerFactoryDataRank {
+	
+	private String rankName;
+	private String ladderName;
+	private int rankId;
+
+	public RankPlayerFactoryDataRank() {
+		super();
+	}
+	public RankPlayerFactoryDataRank( String rankName, String ladderName, int rankId ) {
+		super();
+		
+		this.rankName = rankName;
+		this.ladderName = ladderName;
+		this.rankId = rankId;
+	}
+	
+	public TreeMap<String,Object> getJsonObject(){
+		TreeMap<String,Object> results = new TreeMap<>();
+		
+		results.put("rankName", getRankName());
+		results.put("ladderName", getLadderName());
+		
+		results.put("rankId", Integer.valueOf(rankId));
+		
+		return results;
+	}
+	
+	public String getRankName() {
+		return rankName;
+	}
+	public void setRankName(String rankName) {
+		this.rankName = rankName;
+	}
+	
+	public String getLadderName() {
+		return ladderName;
+	}
+	public void setLadderName(String ladderName) {
+		this.ladderName = ladderName;
+	}
+	
+	public int getRankId() {
+		return rankId;
+	}
+	public void setRankId(int rankId) {
+		this.rankId = rankId;
+	}
+	
+}
